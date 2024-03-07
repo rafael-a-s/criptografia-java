04fe6ccaa2748f09260dc0ffef35014a561a95b4
hem-chrsz <luffychrsz@gmail.com>
2024-03-07 07:15:24 -0300

    feat: base

--- src/main/java/org/example/dicionary/Dicionary.java
+++ src/main/java/org/example/dicionary/Dicionary.java
@@ -1,20 +1,45 @@
 package org.example.dicionary;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Dicionary {
 
     String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    String characters = "yn}#(3e'@yn@hzQ=3<J23<k&urS";
+    String characters = "n}#(3e'@yn@hzQ=3<J23<k&urS";
 
     public String getCharactersForTheIndex(int index) {
-        return this.characters.substring(this.characters.length() -1 ,index);
+        if(index == 0 ) {
+            return String.valueOf(this.characters.charAt(index));
+        }
+        return this.characters.substring(0 ,index + 1);
+    }
+
+    public String getLetterForTheIndex(int index) {
+        return String.valueOf(this.alphabet.charAt(index));
+        
     }
 
     public static void main(String[] args) {
         Dicionary d = new Dicionary();
+        int index1 = d.alphabet.indexOf("R");
+        int index2 = d.alphabet.indexOf("A");
+        int index3 = d.alphabet.indexOf("F");
+        int index4 = d.alphabet.indexOf("A");
+        int index5 = d.alphabet.indexOf("E");
+        int index6 = d.alphabet.indexOf("L");
+        System.out.print(d.getCharactersForTheIndex(index1));
+        System.out.print(d.getCharactersForTheIndex(index2));
+        System.out.print(d.getCharactersForTheIndex(index3));
+        System.out.print(d.getCharactersForTheIndex(index4));
+        System.out.print(d.getCharactersForTheIndex(index5));
+        System.out.print(d.getCharactersForTheIndex(index6));
 
-        System.out.println(d.getCharactersForTheIndex(1));
+        System.out.println();
+
+        System.out.print(d.getLetterForTheIndex(index1));
+        System.out.print(d.getLetterForTheIndex(index2));
+        System.out.print(d.getLetterForTheIndex(index3));
+        System.out.print(d.getLetterForTheIndex(index4));
+        System.out.print(d.getLetterForTheIndex(index5));
+        System.out.print(d.getLetterForTheIndex(index6));
+
     }
 }
